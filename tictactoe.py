6ebdd6270ddc819dd86f0f8562d37f05a06d71a5
Hussain Pettiwala <admin@admins-MacBook-Air.local>
2021-05-06 00:05:41 +0530

    comments added

--- tictactoe.py
+++ tictactoe.py
@@ -29,6 +29,7 @@ def player(board):
 
     x_count = o_count = 0
 
+    # loop over the board to count nuber of Xs and Os
     for sublist in board:
         for variable in sublist:
             if variable == X:
@@ -49,9 +50,14 @@ def actions(board):
     """
     all_actions = set()
 
+    # enumerate is used to obtain index
+    # for list in board
     for i, row in enumerate(board):
+        # loop over values in that list
         for j, element in enumerate(row):
+            # if element is empty means that move can be made
             if element == None:
+                # add that action to the set
                 all_actions.add((i,j))
     
     return all_actions
@@ -61,15 +67,24 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    # the original board should be left unmodified: 
+    # because Minimax will ultimately require considering many different board states during its computation
     board_copy = copy.deepcopy(board)
 
+    # for list in board
     for i, row in enumerate(board_copy):
+        # loop over values in that list
         for j, element in enumerate(row):
+            # if the value being looped over is the action
             if (i, j) == action:
+                # if element is none then the action passed is invalid
                 if element != None:
                     raise Exception("Invalid action")
+                # call the player function to check whose turn it is
+                # and edit the board_copy's action element to that players turn
                 board_copy[i][j] = player(board)
 
+    # return the new board
     return board_copy
 
 
@@ -109,8 +124,11 @@ def winner(board):
 
     # check if all items are same in any list contained in possible_winning_combinations
     for i, row in enumerate(possible_winning_combinations):
+        # if all members of the row are equal then we have a winner
         if all_equal(row):
+            # the winner will be first element of that row
             try:
+                # all indexes of that row will contain the winner (X or O)
                 return row[0]
             except:
                 return None
@@ -127,7 +145,11 @@ def terminal(board):
         return True
 
     # check if the board is full
-    board_list = [item for sublist in board for item in sublist]
+    board_list = [item for sublist in board for item in sublist] # gets values of, list of lists
+
+    # if any no values are None in board then it is  aterminal state
+    # any will return True if x is None
+    # we want x not to be None...
     if not any(x is None for x in board_list):
         return True
     
@@ -169,29 +191,55 @@ def minimax(board):
             values.append(tuple([max_value(result(board, action)), action]))
         return min(values)[1]
  
-# Custom functions
+
 def max_value(board):
+    """
+    Takes a `board` and returns as output the `value` of that board
+    if I am trying to maximize the `value` of that `board`
+    """
+    # I want v to be as high as possible at the end 
+    # so initially I will set it as low as I can
     v = - math.inf
+
+    # if the game is over then 
     if terminal(board):
+        # the utility function will calculate the value of the board
         return utility(board)
 
+    # loop over all possible actions on the board
     for action in actions(board):
-        v = max(v, min_value(result(board, action)))
+        # v should be the maximum of v AND the best that the min player can do
+        v = max(  v, min_value( result(board, action) )  )
 
     return v
 
 def min_value(board):
+    """
+    Takes a `board` and returns as output the `value` of that board
+    if I am trying to minimize the `value` of that `board`
+    """
+    # I want v to be as low as possible at the end 
+    # so initially I will set it as high as I can
     v = math.inf
+    
+    # if the game is over then 
     if terminal(board):
+        # the utility function will calculate the value of the board
         return utility(board)
 
+    # loop over all possible actions on the board
     for action in actions(board):
-        v = min(v, max_value(result(board, action)))
+        # v should be the minimum of v AND the best that the max player can do
+        v = min(  v, max_value( result(board, action) )  )
 
     return v
 
-# takes list or tuple as input and returns
+
 def all_equal(iterator):
+    """
+    Returns True if in given `iterator` (list, tuple etc.) all `values` 
+    are `equal` else reutns False
+    """
     iterator = iter(iterator)
     try:
         first = next(iterator)
